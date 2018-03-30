3cd4c94b444f21b0e1efb820747e2c4e030c03c6
jerry0926 <dream84926@yahoo.com.tw>
2018-03-30 17:51:46 +0800

    新和春修補

---
+++ PGCode/Work.WebMvcApp/Controllers/InternetShopController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace DotWeb.WebApp.Controllers
+{
+    public class InternetShopController : WebFrontController
+    {
+        // GET api/<controller>
+        public ActionResult InternetShop()
+        {
+            ViewBag.BodyClass = "InternetShop";
+            return View();
+        }
+    }
+}
